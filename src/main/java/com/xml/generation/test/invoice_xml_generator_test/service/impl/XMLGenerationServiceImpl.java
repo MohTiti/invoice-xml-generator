c63d648e951a5a34b8a71108715aa02ac0ed3b29
MohTiti <mohammad.t@b-tek.dev>
2026-04-21 19:03:36 +0300

    comment out the invoiceCounter in xml

--- src/main/java/com/xml/generation/test/invoice_xml_generator_test/service/impl/XMLGenerationServiceImpl.java
+++ src/main/java/com/xml/generation/test/invoice_xml_generator_test/service/impl/XMLGenerationServiceImpl.java
@@ -85,7 +85,7 @@ public class XMLGenerationServiceImpl implements XMLGenerationService {
             model.put("invoiceTypeValue", "383");
         }
 
-        model.put("invoiceCounter", Integer.valueOf(invoiceDTO.getInvoiceNumber().replace("EIN", "")));
+//        model.put("invoiceCounter", Integer.valueOf(invoiceDTO.getInvoiceNumber().replace("EIN", "")));
         model.put("currencyEnum", invoiceDTO.getCurrencyEnum());
 
         if(invoiceDTO.getBuyerDTO() != null) {
